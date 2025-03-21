ac8fb854a9247923d99a66f73c36b0ba9e68a771
Marvin Bentley II <truevox@gmail.com>
2025-03-21 17:19:53 -0400

    🔧 Refactor:Move jump-related functions to jumpsled.js and include script in index.html

--- js/mechanics.js
+++ js/mechanics.js
@@ -284,48 +284,4 @@ function updateMechanics(deltaTime) {
     }
   }
   
-  // ------------------- Helper Functions -------------------
-  
-  function onPlayerJumpStart() {
-    player.jumpStartTime = performance.now();
-    player.jumpStartY = player.absY;
-    player.jumpPeakY = player.absY;
-    console.log("Jump initiated at Y:", player.jumpStartY.toFixed(1));
-    unlockAudioContext();
-    jumpOsc = audioCtx.createOscillator();
-    jumpGain = audioCtx.createGain();
-    jumpOsc.type = "sine";
-    jumpGain.gain.setValueAtTime(0.3, audioCtx.currentTime);
-    jumpOsc.connect(jumpGain);
-    jumpGain.connect(audioCtx.destination);
-    jumpOsc.start();
-    
-    // Drain stamina on jump initiation
-    stamina.drainJump();
-  }
-  
-  function onPlayerJumpPeak() {
-    console.log("Reached peak of jump.");
-    // Optionally add a sound effect here.
-  }
-  
-  function cleanupJumpSound() {
-    if (jumpOsc) {
-      jumpOsc.stop();
-      jumpOsc.disconnect();
-      jumpOsc = null;
-    }
-    if (jumpGain) {
-      jumpGain.disconnect();
-      jumpGain = null;
-    }
-  }
-  
-  function onPlayerLand() {
-    const jumpTime = (performance.now() - player.jumpStartTime) / 1000;
-    const jumpHeight = player.jumpPeakY - player.jumpStartY;
-    const totalDistance = player.absY - player.jumpStartY;
-    console.log(`Jump complete! Time: ${jumpTime.toFixed(2)}s, Peak Height: ${jumpHeight.toFixed(1)}, Distance: ${totalDistance.toFixed(1)}`);
-    cleanupJumpSound();
-    stamina.resetJumpTrigger();
-  }
+  // Note: Jump-related functions moved to jumpsled.js
